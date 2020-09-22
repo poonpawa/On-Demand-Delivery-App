0aac2f46b1f4616e08fed3cb8d6732586d8df98f
Poonam Pawar <poonampawar0508@gmail.com>
2020-09-23 00:41:08 +0100

    Added delivered screen content

--- OnDemandDelivery/screens/delivered.js
+++ OnDemandDelivery/screens/delivered.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 
-const delivered = () => {
+const delivered = (props) => {
     return (
         <View style={styles.container}>
-            <Text>delivered</Text>
+            <Image
+                source={require('../assets/Icons/OrderDelivered.png')}
+                style={{ width: 100, height: 100 }}
+            />
+            <Text style={styles.title}>Order Delivered</Text>
+            <Text>Your Order was Delivered</Text>
+            <Text style={styles.linkText} onPress={() => props.navigation.navigate('Home')}>Deliver another order</Text>
         </View>
     )
 }
@@ -16,5 +22,11 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center"
+    },
+    title: {
+        fontFamily: 'NunitoSans-SemiBold'
+    },
+    linkText: {
+        color: '#C75300'
     }
 })
