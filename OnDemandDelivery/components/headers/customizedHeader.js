28af118e89526d3e63ca355e4dd8e412bbff7233
Sumit Narang <sumitnarang.web@gmail.com>
2020-10-19 22:06:41 +0100

    top header css

--- OnDemandDelivery/components/headers/customizedHeader.js
+++ OnDemandDelivery/components/headers/customizedHeader.js
@@ -1,11 +1,22 @@
 
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native'
 import { Header, Icon } from 'react-native-elements';
 import CartIcon from '../cartIcon';
 
+    
 
+  
 const modalHeader = (props) => {
+    const renderCustomLeftIcon = () => {
+        return(
+            <TouchableOpacity onPress={() => props.navigation.goBack()}>
+                <Image
+                source={require('../../assets/Images/backbtn.png')}
+                />
+            </TouchableOpacity>
+        );
+    };
     return (
         <View>
             <Header
@@ -13,25 +24,21 @@ const modalHeader = (props) => {
                     if (props.isRight && props.isSearch) {
                         return (
                             <View style={styles.rightComponent}>
-                                <Icon name='search' color='#fff' type="Ionicons" size={28} />
+                                {/* <Icon name='search' color='#6A748A' type="Ionicons" size={28} /> */}
                                 <CartIcon navigate={props.navigation.navigate} />
                             </View>
                         )
                     } else if (props.isSearch) {
                         return (
                             <View style={styles.rightComponent}>
-                                <Icon name='search' color='#fff' type="Ionicons" size={28} />
+                                {/* <Icon name='search' color='#6A748A' type="Ionicons" size={28} /> */}
                             </View>
                         )
                     }
                 }}
-                centerComponent={{ text: props.title, style: { color: '#fff' } }}
-                leftComponent={{
-                    icon: 'close',
-                    color: '#fff',
-                    onPress:
-                        () => props.navigation.goBack()
-                }}
+                
+                centerComponent={{ text: props.title, style: { color: '#383F51', fontFamily: "NunitoSans-Bold", fontSize: 18 } }}
+                leftComponent={() => renderCustomLeftIcon()}
                 containerStyle={styles.mainContainer}
             />
         </View>
@@ -42,8 +49,10 @@ export default modalHeader
 
 const styles = StyleSheet.create({
     mainContainer: {
-        height: 55,
-        padding: 10
+        paddingLeft: 16,
+        paddingRight: 16,
+        backgroundColor: 'white',
+        borderBottomColor: '#F3F3F3',
     },
     rightComponent: {
         flex: 1,
